d96cb91d673c063d393166cb826be176b3bde91b
fbrennanbohm <felix.brennanbohm@gmail.com>
2020-07-13 14:40:09 +1000

    Added and configured Swagger

--- Startup.cs
+++ Startup.cs
@@ -27,10 +27,25 @@ namespace TodoApi
             services.AddDbContext<TodoContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("TodoConnection")));
 
+            //REgister the Swagger generator
+            services.AddSwaggerGen();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,6 +61,8 @@ namespace TodoApi
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
